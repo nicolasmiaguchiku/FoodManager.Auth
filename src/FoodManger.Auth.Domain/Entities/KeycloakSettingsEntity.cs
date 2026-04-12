fce45fbb8279a6fe693a8558e6a4bbee3ef9b8b1
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-04-12 01:10:35 -0300

    feat: this commit create class keycloak settings entitie

---
+++ src/FoodManger.Auth.Domain/Entities/KeycloakSettingsEntity.cs
@@ -0,0 +1,12 @@
+﻿using FoodManager.Internal.Shared.Models;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FoodManager.Auth.Domain.Entities
+{
+    [BsonIgnoreExtraElements]
+    public class KeycloakSettingsEntity : KeycloakSettings
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+    }
+}
