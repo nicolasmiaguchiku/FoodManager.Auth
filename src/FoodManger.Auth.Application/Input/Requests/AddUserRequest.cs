77653db20c6adee36bbfa2292302937b38768659
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-04-12 00:49:15 -0300

    feat: create class AddUserRequest

---
+++ src/FoodManger.Auth.Application/Input/Requests/AddUserRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodManager.Auth.Application.Input.Requests;
+
+public record AddUserRequest(
+    string Username,
+    string Password,
+    string Email,
+    string FirstName,
+    string LastName,
+    Dictionary<string, string[]> Attributes);
