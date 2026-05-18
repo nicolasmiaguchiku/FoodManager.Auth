b10fcbef73d8aafadf7badcadecee351d0511cfa
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-05-17 23:55:03 -0300

    feat: this commit created realm entity

---
+++ src/FoodManger.Auth.Domain/Entities/RealmEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodManager.Auth.Domain.Entities
+{
+    public class RealmEntity
+    {
+        public required string Realm { get; set; }
+        public string RealmName { get; set; } = string.Empty;
+        public bool Enabled { get; set; }
+    }
+}
